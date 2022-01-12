758a1e87bf83705f6f55da5dff9f668202c92668
Esinko <34211401+Esinko@users.noreply.github.com>
2022-01-12 18:33:38 +0200

    fix: Use initialization vector for encryption

--- src/encrypt.js
+++ src/encrypt.js
@@ -1,14 +1,16 @@
 const { createReadStream, createWriteStream } = require("fs")
-const { createCipheriv } = require("crypto")
+const { createCipheriv, createHash, randomBytes } = require("crypto")
 
 /**
  * Encrypt a file
  * @param {string} filepath
- * @returns {tring} Encrypted output file path
+ * @returns {string} Encrypted output file path
  */
 module.exports = (filepath) => new Promise((resolve) => {
     const input = createReadStream(filepath)
     const output = createWriteStream(`${filepath}.encrypted`)
-    input.pipe(createCipheriv("aes-256-cbc", process.env.ENCRYPTION_KEY)).pipe(output)
-    output.on("finish", resolve(`${filepath}.encrypted`))
+    const iv = randomBytes(16)
+    const cipher = createHash("sha256").update(process.env.ENCRYPTION_KEY).digest("base64").substr(0, 32)
+    input.pipe(createCipheriv("aes-256-cbc", cipher, iv)).pipe(output)
+    output.on("finish", resolve(`${iv}-${filepath}.encrypted`))
 })
