ecc41319b90613ef4577cee84fc81082486ab60c
Esinko <34211401+Esinko@users.noreply.github.com>
2022-01-20 10:23:19 +0200

    fix: Correctly pipe decrypted data

--- src/decrypt.js
+++ src/decrypt.js
@@ -14,13 +14,12 @@ module.exports = (filepath) => new Promise((resolve) => {
     let iv
     const cipher = createHash("sha256").update(process.env.ENCRYPTION_KEY).digest("base64").substr(0, 32)
     let decryptStream
-    input.on("data", (chunk) => {
-        if (iv === undefined) {
-            iv = chunk.slice(0, 16)
-            decryptStream = createDecipheriv("aes-256-cbc", cipher, iv)
-            decryptStream.pipe(output)
-            decryptStream.write(chunk.slice(16))
-        } else decryptStream.write(chunk)
+    input.once("data", (chunk) => {
+        iv = chunk.slice(0, 16)
+        decryptStream = createDecipheriv("aes-256-cbc", cipher, iv)
+        decryptStream.write(chunk.slice(16))
+        input.pipe(decryptStream)
+        decryptStream.pipe(output)
     })
-    output.on("finish", resolve(`${filepath}.decrypted`))
+    output.on("finish", () => { resolve(newPath) })
 })
